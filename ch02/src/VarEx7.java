7da2325e2d5c9b5ec73c89ff358cb620f592d2f9
Chanbeom <kin280000@gmail.com>
2022-05-04 15:21:32 +0900

    20220504

---
+++ ch02/src/VarEx7.java
@@ -0,0 +1,13 @@
+public class VarEx7 {
+    public static void main(String[] args) {
+        int num = 0;
+
+        for (int i = 1; i < 20; i++) {
+//            System.out.println(Math.random());
+//            System.out.println(Math.random() * 10);
+//            System.out.println((int) (Math.random() * 10));
+//            System.out.println((int) (Math.random() * 10) + 1);
+            System.out.println((int) (Math.random() * 11) - 5);
+        }
+    }
+}
