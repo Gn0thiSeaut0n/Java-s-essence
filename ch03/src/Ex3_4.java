2a8857e46ee43770e2565ee308090ad4afab0ee5
Chanbeom <kin280000@gmail.com>
2022-05-18 17:56:56 +0900

    20220518

---
+++ ch03/src/Ex3_4.java
@@ -0,0 +1,20 @@
+public class Ex3_4 {
+    public static void main(String[] args) {
+        Data d = new Data();
+        d.x = 10;
+        System.out.println("main() : x = " + d.x);
+
+        change(d.x);
+        System.out.println("After change(d.x)");
+        System.out.println("main() : x = " + d.x);
+    }
+
+    static void change(int x) {
+        x = 1000;
+        System.out.println("change() : x = " + x);
+    }
+}
+
+class Data {
+    int x;
+}
