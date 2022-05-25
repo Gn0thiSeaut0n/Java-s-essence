7a44cdfb25e5a673362593a68ea6b75bf45be722
Chanbeom <kin280000@gmail.com>
2022-05-25 09:24:08 +0900

    20220525

---
+++ ch04/src/Ex4_2.java
@@ -0,0 +1,27 @@
+public class Ex4_2 {
+    public static void main(String[] args) {
+        Circle c = new Circle();
+        c.p.x = 1;
+        c.p.y = 2;
+        c.r = 3;
+
+        System.out.println("c.p.x = " + c.p.x);
+        System.out.println("c.p.y = " + c.p.y);
+        System.out.println("c.r = " + c.r);
+
+    }
+}
+
+class MyPoint {
+    int x;
+    int y;
+}
+
+//class Circle extends MyPoint {
+//    int r;
+//}
+
+class Circle {
+    MyPoint p = new MyPoint();
+    int r;
+}
