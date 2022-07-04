f0c07a178ec932f4a7b3b858c16579d40058ad5f
Chanbeom <kin280000@gmail.com>
2022-07-04 11:40:30 +0900

    20220704

---
+++ ch07/src/Ex7_03.java
@@ -0,0 +1,27 @@
+import java.util.Calendar;
+
+public class Ex7_03 {
+    public static void main(String[] args) {
+        final int[] TIME_UNIT = {3600, 60, 1};
+        final String[] TIME_UNIT_NAME = {"시간", "분", "초"};
+
+        Calendar time1 = Calendar.getInstance();
+        Calendar time2 = Calendar.getInstance();
+
+
+        time1.set(Calendar.HOUR_OF_DAY, 10);
+        time1.set(Calendar.MINUTE, 20);
+        time1.set(Calendar.SECOND, 1);
+
+        long difference = Math.abs(time2.getTimeInMillis() - time1.getTimeInMillis()) / 1000;
+        System.out.println("time1과 time2의 차이는 " + difference + "초 입니다.");
+
+        String tmp = "";
+        for (int i = 0; i < TIME_UNIT.length; i++) {
+            tmp += difference / TIME_UNIT[i] + TIME_UNIT_NAME[i];
+            difference %= TIME_UNIT[i];
+        }
+
+        System.out.println("시분초로 변환하면 " + tmp + "입니다.");
+    }
+}
---
+++ ch07/src/Ex7_04.java
@@ -0,0 +1,5 @@
+public class Ex7_04 {
+    public static void main(String[] args) {
+
+    }
+}
