699e89f9615cd52667b3b3628add36bc466ae9f3
Chanbeom <kin280000@gmail.com>
2022-07-22 10:49:59 +0900

    20220722

---
+++ ch09/src/GenericTest.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class GenericTest {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+
+        System.out.println(list);
+    }
+}
