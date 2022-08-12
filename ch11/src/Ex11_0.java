188cbb509ac0155d435bcb3724814195db16a779
Chanbeom <kin280000@gmail.com>
2022-08-12 09:08:41 +0900

    20220812

--- ch11/src/Ex11_0.java
+++ ch11/src/Ex11_0.java
@@ -1,14 +1,14 @@
 public class Ex11_0 {
     public static void main(String[] args) {
 //        Object obj = (a, b) -> a > b ? a : b;
-//        MyFunction f = new MyFunction() {
+//        MyFunction1 f = new MyFunction1() {
 //            public int max(int a, int b){
 //                return a > b ? a : b;
 //            }
 //        };
 
         // 람다식(익명 객체)을 다루기 위한 참조변수의 타입은 함수형 인터페이스로 한다.
-        MyFunction f = (a, b) -> a > b ? a : b;
+        MyFunction1 f = (a, b) -> a > b ? a : b;
 
         int value = f.max(3, 5); // 에러, 함수형 인터페이스
         System.out.println(value);
@@ -16,6 +16,6 @@ public class Ex11_0 {
 }
 
 @FunctionalInterface // 안 써도 되지만 추상메서드가 두 개 이상이면 에러가 나도록 달아줌
-interface MyFunction{
+interface MyFunction1{
     int max(int a, int b);
 }
